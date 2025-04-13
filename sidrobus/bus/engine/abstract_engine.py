c45a381deefb3392db06d7d63aa8039a5c0812db
Daniel Castaño Rodríguez <UO288241@uniovi.es>
2025-04-13 22:25:00 +0200

    Create `AbstractEngine` class.
    Create `AbstractEngine` class to serve as a base for different types of
    engines, such as fuel engines and electric engines. This class defines
    the basic properties and methods that any engine should implement.

---
+++ sidrobus/bus/engine/abstract_engine.py
@@ -0,0 +1,23 @@
+"""Module containing the abstract class for engines."""
+
+from abc import ABC, abstractmethod
+
+
+class AbstractEngine(ABC):
+    """Abstract class representing an engine.
+
+    This class serves as a base for different types of engines, such as fuel engines and
+    electric engines. It defines the basic properties and methods that any engine should
+    implement.
+    """
+
+    _efficiency: float
+    _mass: float
+    _energy: float
+    _capacity: float
+
+    @abstractmethod
+    @property
+    def mass(self) -> float:
+        """Returns the mass of the engine."""
+        return self._mass
