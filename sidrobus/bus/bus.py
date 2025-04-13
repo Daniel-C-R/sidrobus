e8f06c83ba1fb3afbf29ea81478d61419b6afcb3
Daniel Castaño Rodríguez <UO288241@uniovi.es>
2025-04-13 22:29:59 +0200

    Create `Bus` class.
    Create a `Bus` class that respresents a bus. Currently, it only has some
    parameters of the bus.

---
+++ sidrobus/bus/bus.py
@@ -0,0 +1,33 @@
+"""Module for Bus class."""
+
+from sidrobus.bus.engine import AbstractEngine
+
+
+class Bus:
+    """Class representing a bus."""
+
+    _engine: AbstractEngine
+    _mass: float
+    _frontal_area: float
+    _aerodynamic_drag_coef: float
+    _rolling_resistance_coef: float
+
+    def __init__(
+        self,
+        engine: AbstractEngine,
+        frontal_area: float,
+        mass: float,
+    ) -> None:
+        """Initializes a Bus object with an engine, frontal area, and mass.
+
+        Args:
+            engine (AbstractEngine): The engine of the bus.
+            frontal_area (float): The frontal area of the bus.
+            mass (float): The mass of the bus.
+
+        Returns:
+            None
+        """
+        self._engine = engine
+        self._frontal_area = frontal_area
+        self._mass = mass
