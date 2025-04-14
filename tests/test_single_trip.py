9f4b4c192e782f202ccdf37fc1c5ddd99517a27f
Daniel Castaño Rodríguez <UO288241@uniovi.es>
2025-04-14 17:19:42 +0200

    Initialize tests for a signle trip.
    Intiralize the file for testing the physical calcuations of a single
    trip. Currently it only tests that the calculation of the distances
    between the points is correct.

---
+++ tests/test_single_trip.py
@@ -0,0 +1,43 @@
+"""Test the calculations for a single trip."""
+
+import numpy as np
+import pytest
+
+from sidrobus.bus.bus import Bus
+from sidrobus.bus.engine.fuel_engine import FuelEngine
+from sidrobus.route import Route
+
+
+@pytest.fixture
+def test_route() -> Route:
+    """Fixture to generate an example route."""
+    longitudes = np.array([0, 1, 2, 3, 2])
+    latitudes = np.array([0, 2, 1, 2, 5])
+    heights = np.array([0, 1, 3, 0, 2])
+    times = np.array([1, 2, 3, 4, 5])
+    velocities = np.array([0, 1, 3, 4, 2])
+
+    return Route(longitudes, latitudes, heights, times, velocities)
+
+
+@pytest.fixture
+def test_fuel_bus() -> Bus:
+    """Fixture to generate an example fuel bus."""
+    engine = FuelEngine(
+        efficiency=0.2,
+        mass=19500,
+    )
+    return Bus(
+        engine=engine,
+        frontal_area=10,
+        mass=5000,
+        aerodynamic_drag_coef=0.3,
+        rolling_resistance_coef=0.01,
+    )
+
+
+def test_route_distances_calculation(test_route: Route) -> None:
+    """Test the route distances calculation."""
+    route = test_route
+    expected_distances = np.array([2.449489743, 2.449489743, 3.31662479, 3.741657387])
+    np.testing.assert_array_almost_equal(route.distances, expected_distances)
