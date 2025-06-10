25afcea3d1b4aae9f70051a45e7fd368f70c3c77
Daniel Castaño Rodríguez <UO288241@uniovi.es>
2025-06-10 23:06:46 +0200

    Reorganize web app in multiple pages

---
+++ web_app/Sidrobus.py
@@ -0,0 +1,15 @@
+"""Start page for Sidrobus web app."""
+
+import streamlit as st
+
+st.title("Sidrobus")
+
+st.markdown(
+    """
+    "On February 28, 1956, the first two buses arrived in Oviedo to begin replacing the
+    tram. These were two Pegasos that operated on the Colloto - Cruce de las Caldas
+    line, and the local humor dubbed them **sidrobuses** due to their popular use for
+    going to drink sidra (cider) in Colloto." - [Transportes Unidos de Asturias,
+     Historia](https://www.tua.es/es/tua/historia/)
+    """
+)
